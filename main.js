6540a0e200147ba929ef4e27c38914e8f2357a85
Marcos-Emanoel-dos-Santos <marcosssantos1997@gmail.com>
2025-10-12 01:48:48 -0300

    Cópia de links encurtado funcionando.

--- main.js
+++ main.js
@@ -3,14 +3,28 @@ function gerarLink(){
     const input_link = document.getElementById('input-link_encurtado').value;
 
     const output_p = output_div.querySelector('p');
+    const output_img = output_div.querySelector('img');
     
-    var output_str = 'https://www.wknot.' + input_link.slice(12, 17);
-    
-    output_div.style = 'display: block;';
+    var output_str = 'https://www.wknot.' + input_link.slice(12, 17) + '.com';
+
 
     output_div.classList.remove('show');
     void output_div.offsetWidth;
     output_div.classList.add('show');
     
     setTimeout(() => {output_p.textContent = output_str}, 550);
+    setTimeout(() => {output_img.style = 'display: inline-block;'}, 550);
+    
+}
+
+const botaoCopiar = document.getElementById('botao_copiar_link');
+botaoCopiar.addEventListener('click', copiaCtdBotao);
+
+function copiaCtdBotao(){
+    const output_p = document.getElementById('output-link_encurtado-p').textContent;
+    if (!output_p) {
+        return;
+    }
+    
+    navigator.clipboard.writeText(output_p)
 }
