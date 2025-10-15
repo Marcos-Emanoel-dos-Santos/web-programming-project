87db01ade9109f3048135dc4975c024da41c3216
Marcos-Emanoel-dos-Santos <marcosssantos1997@gmail.com>
2025-10-15 19:36:50 -0300

    qwfuoheweiçjghweiu

--- js/main.js
+++ js/main.js
@@ -68,6 +68,7 @@ if (nomeDoArquivo == 'signup') {
 
 			if (isFormValid) {
 				console.log('Form valid, sending data.');
+				window.location.href = "index.html";
 			} else {
 				console.log('Form invalid, please check the fields.');
 			}
@@ -117,5 +118,53 @@ if (nomeDoArquivo == 'signup') {
 		const isValidPsswd = validatePsswd();
 
 		return isValidName && isValidEmail && isValidPsswd;
+	}
+}
+
+if(nomeDoArquivo == 'signin'){
+	document.addEventListener('DOMContentLoaded', () => {
+		const form = document.getElementById('signinForm');
+
+		form.addEventListener('submit', function(event) {
+			event.preventDefault();
+
+			const isFormValid = validateForm();
+
+			if (isFormValid) {
+				console.log('Form valid, sending data.');
+				window.location.href = "index.html";
+			} else {
+				console.log('Form invalid, please check the fields.');
+			}
+		});
+	});
+
+
+	function validateEmail(){
+		const email = document.getElementById('email').value.trim();
+
+		// regex for email validation (eu totalmente fiz isso de cabeça)
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		if (!emailRegex.test(email)) {
+			document.getElementById('emailError').textContent = 'Please enter a valid email address.';
+			return false;
+		}
+		return true;
+	}
+
+	function validatePsswd(){
+		const password = document.getElementById('password').value.trim();
+
+		// VALIDAÇÃO DA SENHA NO LOGIN (PRECISA DE BANCO DE DADOS)
+
+		return true;
+	}
+
+	function validateForm() {
+		document.querySelectorAll('.error').forEach(e => e.textContent = '');
+		const isValidEmail = validateEmail();
+		const isValidPsswd = validatePsswd();
+
+		return isValidEmail && isValidPsswd;
 	}
 }
